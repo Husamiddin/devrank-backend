7bcd228147b21474a1c256877546f0c537406439
Husamiddin <aminovhusammiddin@gmail.com>
2026-08-31 22:46:56 +0500

    Fix Prisma Client import for ESM

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,6 +1,9 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
 import { PrismaPg } from "@prisma/adapter-pg";
+
 if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is required");
+
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 export const prisma = new PrismaClient({ adapter });
