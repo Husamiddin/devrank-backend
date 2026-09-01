77fd7ac71a96b3da9be2144f4a9d288614fd6108
Husamiddin <aminovhusammiddin@gmail.com>
2026-09-01 21:06:41 +0500

    Clean up prisma.js and remove heavy adapters

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,9 +1,3 @@
-import "dotenv/config";
-import pkg from "@prisma/client";
-const { PrismaClient } = pkg;
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
-if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is required");
-
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient();
